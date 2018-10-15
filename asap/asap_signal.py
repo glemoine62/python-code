7f2abd226954cd7ce11f8b1de7aff3496283d27a
glemoine62 <guido.lemoine@ec.europa.eu>
2018-10-15 14:00:35 +0200

    asap chip update

--- asap/asap_signal.py
+++ asap/asap_signal.py
@@ -1,4 +1,32 @@
 import ee
+import modis_mask as mm
+
+def getS1Signal(request):
+
+	pt = ee.Geometry.Point(float(request.get('lon')), float(request.get('lat')))
+    
+	#cloud_pcent = int(request.get('cloudpcent'))
+
+	deltad = int(request.get('deltad'))
+	end_date = ee.Date(request.get('end_period'))
+	start_date = end_date.advance(-deltad, 'day')
+    
+	s1 = ee.ImageCollection('COPERNICUS/S1_GRD_FLOAT').filterBounds(pt).\
+	  filterDate(start_date, end_date).sort('system:time_start')
+
+	if s1.size().getInfo() == 0:
+		return []
+	else:
+		s1 = s1.map(lambda img: img.addBands([ee.Image.constant(img.get('system:time_start')).rename(['tstamp'])]))
+
+		composites = ee.List(s1.aggregate_array('system:time_start')).reduce(ee.Reducer.frequencyHistogram())
+
+		s1comp = ee.ImageCollection(ee.Dictionary(composites).keys().map(lambda k:  
+  			ee.Image(s1.filter(ee.Filter.equals('system:time_start', ee.Number.parse(k))).mosaic())))
+
+		profile = s1comp.select(['tstamp', 'VV', 'VH']).map(lambda img: img.set('stats', img.reduceRegion(ee.Reducer.mean(), pt.buffer(20.0, 1.0).bounds(1.0), 10.0)))
+
+		return profile.aggregate_array('stats').getInfo()
 
 def getS2Signal(request):
 
@@ -14,11 +42,20 @@ def getS2Signal(request):
 	  filterMetadata('CLOUDY_PIXEL_PERCENTAGE', 'less_than', cloud_pcent).\
 	  filterDate(start_date, end_date).sort('system:time_start')
 
-	s2 = s2.map(lambda img: img.addBands([img.normalizedDifference(['B8', 'B4']).rename('NDVI'), ee.Image.constant(img.get('system:time_start')).rename('tstamp')]))
+	if s2.size().getInfo() == 0:
+		return []
+	else:
+		s2 = s2.map(lambda img: img.addBands([img.normalizedDifference(['B8', 'B4']).rename(['NDVI']), ee.Image.constant(img.get('system:time_start')).rename(['tstamp'])]))
 
-	profile = s2.select(['tstamp', 'NDVI']).map(lambda img: img.set('stats', img.reduceRegion(ee.Reducer.mean(), pt.buffer(20.0, 1.0).bounds(1.0), 10.0)))
+		composites = ee.List(s2.aggregate_array('system:time_start')).reduce(ee.Reducer.frequencyHistogram())
 
-	return profile.aggregate_array('stats').getInfo()
+		s2comp = ee.ImageCollection(ee.Dictionary(composites).keys().map(lambda k:  
+  			ee.Image(s2.filter(ee.Filter.equals('system:time_start', ee.Number.parse(k))).mosaic())))
+
+		profile = s2comp.select(['tstamp', 'NDVI']).map(lambda img: img.set('stats', img.reduceRegion(ee.Reducer.mean(), pt.buffer(20.0, 1.0).bounds(1.0), 10.0)))
+
+		return profile.aggregate_array('stats').getInfo()
+
 
 def getL8Signal(request):
 
@@ -34,11 +71,15 @@ def getL8Signal(request):
 	  filterMetadata('CLOUD_COVER_LAND', 'less_than', cloud_pcent).\
 	  filterDate(start_date, end_date).sort('system:time_start')
 
-	l8 = l8.map(lambda img: img.addBands([img.normalizedDifference(['B5', 'B4']).rename('NDVI'), ee.Image.constant(img.get('system:time_start')).rename('tstamp')]))
+	if l8.size().getInfo() == 0:
+		return []
+	else:
 
-	profile = l8.select(['tstamp', 'NDVI']).map(lambda img: img.set('stats', img.reduceRegion(ee.Reducer.mean(), pt.buffer(50.0, 1.0).bounds(1.0), 30.0)))
+		l8 = l8.map(lambda img: img.addBands([img.normalizedDifference(['B5', 'B4']).rename(['NDVI']), ee.Image.constant(img.get('system:time_start')).rename(['tstamp'])]))
 
-	return profile.aggregate_array('stats').getInfo()
+		profile = l8.select(['tstamp', 'NDVI']).map(lambda img: img.set('stats', img.reduceRegion(ee.Reducer.mean(), pt.buffer(50.0, 1.0).bounds(1.0), 30.0)))
+
+		return profile.aggregate_array('stats').getInfo()
 
 
 def getMODISSignal(request):
@@ -49,22 +90,31 @@ def getMODISSignal(request):
 	end_date = ee.Date(request.get('end_period'))
 	start_date = end_date.advance(-deltad, 'day')
 
-	mo = ee.ImageCollection('MODIS/006/MOD13Q1').filterBounds(pt).\
-	  filterDate(start_date, end_date).sort('system:time_start')
+	mo = ee.ImageCollection(ee.ImageCollection('MODIS/006/MOD09Q1').merge(ee.ImageCollection('MODIS/006/MYD09Q1'))).\
+	  filterBounds(pt).filterDate(start_date, end_date).sort('system:time_start')
 
-	mo = mo.map(lambda img: img.divide(10000.0).addBands([ee.Image.constant(img.get('system:time_start')).rename('tstamp')]))
+	if mo.size().getInfo() == 0:
+		return []
+	else:
 
-	profile = mo.select(['tstamp', 'NDVI']).map(lambda img: img.set('stats', img.reduceRegion(ee.Reducer.mean(), pt.buffer(100.0, 1.0).bounds(1.0), 250.0)))
+		mo = mo.map(mm.maskPixels)
 
-	return profile.aggregate_array('stats').getInfo()  
+		mo = mo.map(lambda img: img.addBands([img.normalizedDifference(['sur_refl_b02', 'sur_refl_b01']).rename(['NDVI']), ee.Image.constant(img.get('system:time_start')).rename(['tstamp'])]))
+
+		profile = mo.select(['tstamp', 'NDVI']).map(lambda img: img.set('stats', img.reduceRegion(ee.Reducer.mean(), pt.buffer(100.0, 1.0).bounds(1.0), 250.0)))
+
+		return profile.aggregate_array('stats').getInfo()  
 
 if __name__ == '__main__':
 
 	ee.Initialize()
 
-	request = {'lon': '5.6844', 'lat': '52.7073', 'cloudpcent': '10', 'end_period': '2018-09-30', 'deltad': '350'}
+	#request = {'lon': '5.6844', 'lat': '52.7073', 'cloudpcent': '10', 'end_period': '2018-09-30', 'deltad': '35'}
+	request = {'lon': '6.0567', 'lat': '52.5686', 'cloudpcent': '10', 'end_period': '2018-09-30', 'deltad': '35'}
 
-	dict_profile = {'S2': getS2Signal(request)} 
+	dict_profile = {}
+	dict_profile['S1'] = getS1Signal(request)
+	dict_profile['S2'] = getS2Signal(request)
 	dict_profile['L8'] = getL8Signal(request)
 	dict_profile['MODIS'] = getMODISSignal(request)
 
--- asap/asap_signal_plot.py
+++ asap/asap_signal_plot.py
@@ -2,44 +2,52 @@ import ee
 from pandas import DataFrame
 from datetime import datetime
 import matplotlib.pyplot as plt
+import numpy as np
 
-def getSignal(request):
-
-	pt = ee.Geometry.Point(float(request.get('lon')), float(request.get('lat')))
-    
-	cloud_pcent = int(request.get('cloudpcent'))
-
-	deltad = int(request.get('deltad'))
-	end_date = ee.Date(request.get('end_period'))
-	start_date = end_date.advance(-deltad, 'day')
-    
-	s2 = ee.ImageCollection('COPERNICUS/S2').filterBounds(pt).\
-	  filterMetadata('CLOUDY_PIXEL_PERCENTAGE', 'less_than', cloud_pcent).\
-	  filterDate(start_date, end_date).sort('system:time_start')
-
-	s2 = s2.map(lambda img: img.addBands([img.normalizedDifference(['B8', 'B4']).rename('NDVI'), ee.Image.constant(img.get('system:time_start')).rename('tstamp')]))
-
-	profile = s2.select(['tstamp', 'NDVI']).map(lambda img: img.set('stats', img.reduceRegion(ee.Reducer.mean(), pt.buffer(20.0, 1.0).bounds(1.0), 10.0)))
-
-	return profile.aggregate_array('stats').getInfo()
+import asap_signal as asig
 
   
 if __name__ == '__main__':
 
 	ee.Initialize()
 
-	request = {'lon': '5.6844', 'lat': '52.7073', 'cloudpcent': '10', 'end_period': '2018-09-30', 'deltad': '650'}
+	request = {'lon': '5.6844', 'lat': '52.7073', 'cloudpcent': '10', 'end_period': '2018-09-30', 'deltad': '350'}
+	request = {'lon': '34.36357', 'lat': '7.86203', 'cloudpcent': '10', 'end_period': '2018-09-30', 'deltad': '350'}
+ 
+	moprofile = asig.getMODISSignal(request)
 
-	profile = getSignal(request)
-
-	df = DataFrame(profile, columns = ['NDVI', 'tstamp'])
+	df = DataFrame(moprofile, columns = ['NDVI', 'tstamp'])
 
 	df['time'] = df.tstamp.map(lambda t: datetime.fromtimestamp(int(t/1e3)))
 
 	df.set_index(['time'], inplace=True)
 
-	print df
+	s1profile = asig.getS1Signal(request)
 
-	plt.figure()
-	plt.plot(df.index, df['NDVI'])
+	df1 = DataFrame(s1profile, columns = ['VH', 'VV', 'tstamp'])
+
+	df1['time'] = df1.tstamp.map(lambda t: datetime.fromtimestamp(int(t/1e3)))
+
+	df1.set_index(['time'], inplace=True)
+
+	fig, ax1 = plt.subplots()
+	ax1.plot(df.index, df['NDVI'], marker = '^', linestyle = ' ', color = 'blue', label = "MODIS NDVI")
+	ax1.set_xlabel('time (s)')
+
+	ax1.set_ylabel('NDVI', color='blue')	
+	ax1.set_ylim(0, 1)
+	
+	ax2 = ax1.twinx()
+	ax2.set_ylabel('$\sigma^\degree$ (dB)', color='red')	
+
+	ax2.plot(df1.index, 10.0*np.log10(df1['VV']), marker = 'o', linestyle = ' ', color = 'red', label = "S1 VV")
+	ax2.plot(df1.index, 10.0*np.log10(df1['VH']), marker = 'x', linestyle = ' ', color = 'green', label = "S1 VH")
+	ax2.plot(df1.index, 10.0*np.log10(df1['VH']/df1['VV']), marker = 'x', linestyle = ' ', color = 'black', label = "S1 VH/VV")
+	ax2.set_ylim(-25, 0)
+
+	fig.legend()
+	fig.tight_layout()
+
 	plt.show()
+	
+
